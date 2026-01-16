1fdce7a392b921240c3bdce36423d24fd48a6e07
abdulfour5-tech <abdulfour5@gmail.com>
2026-01-16 22:43:59 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,32 +21,25 @@ const msg = document.getElementById('formMsg');
 form.addEventListener('submit', function(e){
   e.preventDefault();
 
-  const name = document.getElementById('name').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const message = document.getElementById('message').value.trim();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
 
-  if(!name || !email || !message){
-    msg.innerText = "Please fill all fields.";
-    msg.style.color = "red";
-    return;
-  }
-
-  msg.innerText = "Sending...";
-  msg.style.color = "white";
-
-  emailjs.send("service_3llvhqo", "template_9xitu6x", {
+  emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
     name: name,
     email: email,
     message: message
   })
-  .then(()=>{
-    msg.innerText = "Message sent successfully!";
-    msg.style.color = "#38bdf8";
-    form.reset();
-  })
-  .catch(()=>{
-    msg.innerText = "Failed to send message.";
-    msg.style.color = "red";
+  .then(function(response) {
+     msg.innerText = "Message sent successfully!";
+     msg.style.color = "#38bdf8";
+     form.reset();
+  }, function(error) {
+     console.log("FAILED...", error);
+     msg.innerText = "Failed to send message.";
+     msg.style.color = "red";
   });
 });
+
+
 
